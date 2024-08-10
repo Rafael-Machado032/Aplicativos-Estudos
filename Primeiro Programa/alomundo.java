253b8b7a08ad9b1a8f4faa1be661ea2d3ce83e3c
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-08-10 13:24:08 -0300

    a

--- Primeiro Programa/alomundo.java
+++ Primeiro Programa/alomundo.java
@@ -2,6 +2,14 @@ public class alomundo { // classe tem que ser o nome do arquivo
    public static void main(String[] args) { // Primeiro classe do programa tem que ser o metodo principal "main"
       // args é um array de string
       // void não retorna nada
-      System.out.println("Ola, mundo!");
+
+      int ano = 2020;
+      String curso = "Java";
+
+      System.out.print("Ola, mundo! \n");
+      System.out.println("Meu nome eh "); //ja corta pra proxima linha
+      System.out.printf("%s e tenho %d anos \n","Rafael Machado",35);
+      System.out.printf("Sou programador %s desde %d ",curso , ano );
+
    }
 }

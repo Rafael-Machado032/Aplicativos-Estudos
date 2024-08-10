ba86957fb25b57db84763e17c7d6ed31e4a58bb0
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2024-08-10 12:01:51 -0300

    a

--- Primeiro Programa/alomundo.java
+++ Primeiro Programa/alomundo.java
@@ -2,6 +2,6 @@ public class alomundo { // classe tem que ser o nome do arquivo
    public static void main(String[] args) { // Primeiro classe do programa tem que ser o metodo principal "main"
       // args é um array de string
       // void não retorna nada
-      System.out.println("Olá, mundo!");
+      System.out.println("Ola, mundo!");
    }
 }

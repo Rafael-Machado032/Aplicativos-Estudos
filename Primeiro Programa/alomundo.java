65b598f545d20ef49588b4ce1e2f1e1e3f6820a8
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-08-10 11:26:32 -0300

    first commit

---
+++ Primeiro Programa/alomundo.java
@@ -0,0 +1,7 @@
+public class alomundo {  //classe tem que ser o nome do arquivo
+ public static void main(String[] args) { //Primeiro classe do programa tem que ser o metodo principal "main"
+ //args é um array de string
+ //void não retorna nada
+    System.out.println("Olá, mundo!");
+ }   
+}

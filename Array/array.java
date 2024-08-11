8c154bf8a682e4bd9a2ac58928fb7f5109a7e9f2
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-08-11 18:28:26 -0300

    a

---
+++ Array/array.java
@@ -0,0 +1,21 @@
+/**
+ * array
+ */
+public class array {
+
+    public static void main(String[] args) {
+        
+        //int [] numero; //Coleção de variaveis um array
+        int [] num = new int [5]; //Um array estaciado 5 posisão
+        
+        num[0] = 10;
+        num[1] = 5;
+        num[2] = 15;
+        num[3] = 35;
+        num[4] = 4;
+
+        System.out.println(num[2]);
+
+
+    }
+}

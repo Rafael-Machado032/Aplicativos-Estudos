ec9f066420a8bf2537b3e786df5ac4ddb865d71a
Rafael-Machado032 <rafaelmfds@gmail.com>
2024-08-11 19:22:25 -0300

    a

--- Array/array.java
+++ Array/array.java
@@ -1,21 +1,49 @@
+import java.util.Scanner;
 /**
  * array
  */
 public class array {
 
     public static void main(String[] args) {
-        
-        //int [] numero; //Coleção de variaveis um array
-        int [] num = new int [5]; //Um array estaciado 5 posisão
-        
-        num[0] = 10;
-        num[1] = 5;
-        num[2] = 15;
-        num[3] = 35;
-        num[4] = 4;
 
-        System.out.println(num[2]);
+        final int tam = 5; //Final eh igual uma constante
+        
+        int [] numero = {10,20,30,40,50,60,70,80,90,100}; //Coleção de variaveis um array
+        int [] num = new int [tam]; //Um array estaciado 5 posisão
+        
+        num[0] = 5;
+        num[1] = 4;
+        num[2] = 3;
+        num[3] = 2;
+        num[4] = 1;
 
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+        for (int i = 0; i < numero.length; i++) {
+            System.out.println(numero[i]);
+        }
+
+
+        char[] gabarito = {'a','a','d','b','c'};
+        char[] resposta = new char[gabarito.length];
+        int nota = 0;
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < gabarito.length; i++) {
+            System.out.printf("Digite a resposta da questão %d: ", i);
+            resposta[i] = scan.nextLine().charAt(0);
+
+        }
+
+        for (int i = 0; i < gabarito.length; i++) {
+            if (resposta[i]==gabarito[i]) {
+                nota++;
+            }
+        }
+
+        System.out.printf("A nota é %d.",nota);
 
     }
 }
